e5724d43ed173e798ff80905629b9a040f66ea6f
nekattrin <negreeva.katerina@gmail.com>
2024-03-20 20:32:06 +0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = r'C:\UcoursePy\app1\ToDo_App\todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Return to-does"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_local, filepath=FILEPATH):
+    """ Writes to-does into the file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_local)
+
+if __name__ == "__main__":
+    print(get_todos())
+
---
+++ web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+
+st.title("To-Do app")
+st.subheader("Hello friend!")
+st.write("This app is to make you nervous")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+
+st.text_input(label='', placeholder='Enter a todo...',
+              on_change=add_todo, key='new_todo')
